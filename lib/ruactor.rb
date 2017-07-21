1bfd815b7f92bb9f3cd2240f2e69d43d6e84bddd
Tim Spence <timothywspence@gmail.com>
2017-07-21 10:44:27 +0100

    POC
    Asynchronous methods can be invoked via send! which queues the object,
    method and arguments on a queue to be invoked by a idspatcher's thread
    pool.

--- lib/ruactor.rb
+++ lib/ruactor.rb
@@ -1,5 +1,41 @@
 require "ruactor/version"
+require 'thread'
+require 'singleton'
 
 module Ruactor
-  # Your code goes here...
+  module Actor
+
+    def send! (name, *args)
+      Dispatcher.instance.queue << [self, name, *args]
+    end
+
+  end
+
+  class Dispatcher
+    include Singleton
+
+    attr_reader :queue
+
+    def initialize()
+      @queue = Queue.new
+    end
+
+    def start
+      @threads = []
+      10.times do
+        thread = Thread.new do
+          loop do
+            obj, method, *args = @queue.pop
+            obj.send method, *args
+          end
+        end
+        @threads << thread
+      end
+    end
+
+    def stop
+      @threads.each { |t| t.exit }
+      @threads.each { |t| t.join }
+    end
+  end
 end
--- spec/ruactor_spec.rb
+++ spec/ruactor_spec.rb
@@ -5,7 +5,33 @@ RSpec.describe Ruactor do
     expect(Ruactor::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "queues call for execution" do
+    class Test
+      include Ruactor::Actor
+
+      def foo
+        puts :foo
+      end
+    end
+
+    x = Test.new
+    x.send! :foo
+    expect(Ruactor::Dispatcher.instance.queue.pop).to eq([x, :foo])
   end
+
+  it "executes tasks in the queue" do
+    x = Object.new
+    x.singleton_class.class_eval do
+      attr_accessor :invoked
+    end
+    x.invoked = false
+
+    Ruactor::Dispatcher.instance.queue << [x, :invoked=, true]
+    Ruactor::Dispatcher.instance.start
+    sleep 2
+    Ruactor::Dispatcher.instance.stop
+
+    expect(x.invoked).to be(true)
+  end
+
 end
